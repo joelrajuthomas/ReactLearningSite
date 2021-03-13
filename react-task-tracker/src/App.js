953c2152b14ace6df39b3259881ed1afa7cad235
joel <yeshui@joels-MacBook-Air.local>
2021-03-13 15:56:04 -0600

    yep

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,13 +1,11 @@
-function App() {
-  const name = 'Brad'
-  const x = true
+import Header from './components/Header'
+
+const App = () => {
   return (
-    <div className="Container">
-      <h1>Hello from React</h1>
-      <h2>Hello {name}</h2>
-      <h3>{x ? 'Yes' : 'No'}</h3>
+    <div className='container'>
+      <Header /> 
     </div>
-  );
+  )
 }
 
 export default App;
